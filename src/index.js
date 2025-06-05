c3441a978f7205d4c6220a3187abd4e2ae7f0d6f
Belinda <bmdebruyn@gmail.com>
2025-06-05 12:00:21 -0600

    button styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * Module dependencies for Highcharts 3D Funnel chart.
  */
-import { symbol } from 'd3';
 import * as Highcharts from 'highcharts';
 import 'highcharts/highcharts-more';
 import 'highcharts/modules/exporting';
@@ -219,7 +218,26 @@ var parseMetadata = metadata => {
                 lang: {
                     thousandsSep: ','
                 },
-                colors: ['#004b8d', '#939598', '#faa834', '#00aa7e', '#47a5dc', '#006ac7', '#ccced2', '#bf8028', '#00e4a7']
+                colors: ['#004b8d', '#939598', '#faa834', '#00aa7e', '#47a5dc', '#006ac7', '#ccced2', '#bf8028', '#00e4a7'],
+                navigation: {
+                    buttonOptions: {
+                        symbolStroke: '#004b8d',  // Outline color
+                        symbolFill: 'transparent', // No fill
+                        symbolStrokeWidth: 1,
+                        // Core button shape settings
+                        height: 32,          // Ensure square for circle
+                        width: 32,
+                        theme: {
+                            r: 16,           // Rounded corners (half width = full circle)
+                            fill: '#f7f7f7', // Background color
+                            stroke: '#ccc',  // Thin outer border
+                            'stroke-width': 0.8,
+                            style: {
+                                cursor: 'pointer'
+                            }
+                        }
+                    }
+                }
             });
 
             const gradientFillColors = [
@@ -277,10 +295,14 @@ var parseMetadata = metadata => {
             });
 
             Highcharts.SVGRenderer.prototype.symbols.contextButton = function (x, y, w, h) {
-                const radius = w * 0.06;
-                const spacing = w * 0.22;
-                const centerY = y + h / 2;
-                const startX = x + (w - 2 * spacing) / 2;
+                const radius = w * 0.11;
+                const spacing = w * 0.4;
+
+                const offsetY = 2;    // moves dots slightly down
+                const offsetX = 1;  // moves dots slightly to the right
+
+                const centerY = y + h / 2 + offsetY;
+                const startX = x + (w - spacing * 2) / 2 + offsetX;
 
                 const makeCirclePath = (cx, cy, r) => [
                     'M', cx - r, cy,
